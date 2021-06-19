78d55c5ef23a806aac3102700eb2729bb92b06ef
Yuli Wang <yuliwa@microsoft.com>
2021-06-20 01:34:18 +0800

    Support branch check-out

--- CloneAllRepos.ps1
+++ CloneAllRepos.ps1
@@ -8,6 +8,7 @@ Get-Content "CloneAllRepos.config" | foreach-object -begin {$h=@{}} -process {
 $url = $h.Get_Item("Url")
 $username = $h.Get_Item("Username")
 $password = $h.Get_Item("Password")
+$dest = $h.Get_Item("Dest")
 
 # Retrieve list of all repositories
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
@@ -24,16 +25,19 @@ $json = convertFrom-JSON $resp.Content
 
 # Clone or pull all repositories
 $initpath = get-location
+$fetchbranch = ("{0}/{1}" -f $initpath, "git-checkout-all-branches.sh")
+chmod +x $fetchbranch
 foreach ($entry in $json.value) { 
     $name = $entry.name 
-    Write-Host $name
+    $proj = ("{0}/{1}" -f $dest, $name)
+    Write-Host $proj
 
     $url = $entry.remoteUrl -replace "://", ("://{0}@" -f $gitcred)
-    if(!(Test-Path -Path $name)) {
-        git clone $url
+    if(!(Test-Path -Path $proj)) {
+        git clone $url $proj
     } else {
-        set-location $name
-        git pull
-        set-location $initpath
+        Set-Location $proj
+        sh $fetchbranch
+        Set-Location $initpath
     }
 }
